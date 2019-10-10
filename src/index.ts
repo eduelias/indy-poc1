3346d8f1ccf97136fbc2bd3a2d622e54ecb46d93
Eduardo Elias Saleh <du7@msn.com>
2019-10-10 12:46:32 +0200

    feat(files): adding linting files
    Signed-off-by: Eduardo Elias Saleh <du7@msn.com>

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export class Issuer {
+  public Issue(somethig: string): Promise<string> {
+    const b = (c: string | PromiseLike<string> | undefined): string => `${c}`;
+    return new Promise((a) => {
+      return `${somethig} ${b('a')} Edu: ${a}`;
+    });
+  }
+}
