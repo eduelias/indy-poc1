b59aabf9d347ff8901542fe19981e5cde320abcf
Eduardo Elias Saleh <du7@msn.com>
2019-10-16 16:05:42 +0200

    feat(disconnect): adding disconnect event
    Signed-off-by: Eduardo Elias Saleh <du7@msn.com>

---
+++ src/services/PoolService.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import mkdirp from 'mkdirp';
+import os from 'os';
+import { join } from 'path';
+//import config from 'node-config-ts';
+
+export class UtilService {
+  private pathAsArray: string[] = [os.tmpdir(), 'indy'];
+
+  public async getPoolGenesisTxnPath(poolName: string): Promise<string> {
+    this.pathAsArray.push(`${poolName}.tx`);
+
+    const path = join(...this.pathAsArray);
+    await this.savePoolGenesisTxnFile(path);
+
+    return path;
+  }
+
+  public async poolGenesisTxnData(): Promise<string> {
+    return ''; // config.genesisTxn
+    // .map((gtx: GenesisTxn) => JSON.stringify(gtx))
+    // .join();
+  }
+
+  public async savePoolGenesisTxnFile(path: string): Promise<void> {
+    const data = await this.poolGenesisTxnData();
+    await this.mkdir(path);
+    return fs.writeFileSync(path, data, 'utf8');
+  }
+
+  public async mkdir(path: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const folderPath = path
+        .split('/')
+        .slice(0, path.split('/').length - 1)
+        .join('/');
+      mkdirp(folderPath, (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+}
