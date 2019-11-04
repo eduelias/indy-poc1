bd94a484b5931d6abc699d12bb2cb3308cbef4aa
Eduardo Elias Saleh <du7@msn.com>
2019-11-04 14:46:09 +0100

    feat: config at url gen
    Signed-off-by: Eduardo Elias Saleh <du7@msn.com>

--- Src/Services/PoCService.ts
+++ Src/Services/PoCService.ts
@@ -2,7 +2,8 @@ import { HtmlServer } from '@Servers/HtmlServer';
 import { SocketServer } from '@Servers/SocketServer';
 import { ConfigSchema, ConfigService } from '@Services/ConfigService';
 import { queryForTx } from '@Services/Indy/PoolerService';
-import { agency } from 'aries-sdk-ts-beta';
+import { RestAgency } from 'aries-sdk-ts-beta';
+import { URL } from 'url';
 // import { Agency } from 'aries-sdk-ts';
 // import { PoolService } from '@Services/PoolService';
 // import { SocketLogger } from '@Services/SocketLogger';
@@ -16,20 +17,18 @@ export class PoCService {
     const htmlServer = new HtmlServer();
     htmlServer.listen(config.port);
 
-    const steward = new agency.RestAgency();
+    const steward = new RestAgency();
     steward.run({
-      url: 'localhost',
-      port: 3000,
+      url: new URL('http://localhost:3000'),
       label: 'Steward',
       walletName: 'Steward Wallet',
       walletKey: 'steward_key',
       publicDidSeed: '000000000000000000000000Steward1',
     });
 
-    const government = new agency.RestAgency();
+    const government = new RestAgency();
     government.run({
-      url: 'localhost',
-      port: 3001,
+      url: new URL('http://localhost:3001'),
       label: 'Government',
       walletName: 'governmentWallet',
       walletKey: 'government_key',
