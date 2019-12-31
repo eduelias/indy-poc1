1c7c3162b8acde66a1a2964c3f9e84236c41017d
Eduardo Elias Saleh <du7@msn.com>
2019-12-31 14:55:54 +0100

    feat(latest): adding get latest block
    Signed-off-by: Eduardo Elias Saleh <du7@msn.com>

--- Src/Services/Indy/PoolerService.ts
+++ Src/Services/Indy/PoolerService.ts
@@ -1,9 +1,27 @@
 import { IndyReq, ledgerType, type } from './Explorer';
+import sizeof from 'object-sizeof';
+
+export const ledgers = ['POOL', 'CONFIG', 'DOMAIN'];
 
 export const db = {
   POOL: [],
   DOMAIN: [],
   CONFIG: [],
+};
+
+/**
+ * This function will break a dataset reading job by
+ * a predicted size and a batch-size "step"
+ *
+ * @param cb The callback that will be executed sliced
+ * @param size The predicted size of the whole batch
+ * @param step The step size
+ */
+const buildPromises = (cb, size, step = 50): any[] => {
+  return Array.from(
+    { length: Math.ceil(size / step) },
+    (v, i) => i * step,
+  ).map((i) => cb(i));
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -15,6 +33,7 @@ const getIndy = (): any =>
     genesisTxn:
       //  '{"reqSignature":{},"txn":{"data":{"data":{"alias":"FoundationBuilder","blskey":"3gmhmqpPLqznZF3g3niodaHjbpsB6TEeE9SpgXgBnZJLmXgeRzJqTLajVwbhxrkomJFTFU4ohDC4ZRXKbUPCQywJuPAQnst8XBtCFredMECn4Z3goi1mNt5QVRdU8Ue2xMSkdLpsQMjCsNwYUsBguwXYUQnDXQXnHqRkK9qrivucQ5Z","blskey_pop":"RHWacPhUNc9JWsGNdmWYHrAvvhsow399x3ttNKKLDpz9GkxxnTKxtiZqarkx4uP5ByTwF4kM8nZddFKWuzoKizVLttALQ2Sc2BNJfRzzUZMNeQSnESkKZ7U5vE2NhUDff6pjANczrrDAXd12AjSG61QADWdg8CVciZFYtEGmKepwzP","client_ip":"35.161.146.16","client_port":"9702","node_ip":"50.112.53.5","node_port":"9701","services":["VALIDATOR"]},"dest":"GVvdyd7Y6hsBEy5yDDHjqkXgH8zW34K74RsxUiUCZDCE"},"metadata":{"from":"V5qJo72nMeF7x3ci8Zv2WP"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"fe991cd590fff10f596bb6fe2362229de47d49dd50748e38b96f368152be29c7"},"ver":"1"}',
       '{"reqSignature":{},"txn":{"data":{"data":{"alias":"ev1","client_ip":"54.207.36.81","client_port":"9702","node_ip":"18.231.96.215","node_port":"9701","services":["VALIDATOR"]},"dest":"GWgp6huggos5HrzHVDy5xeBkYHxPvrRZzjPNAyJAqpjA"},"metadata":{"from":"J4N1K1SEB8uY2muwmecY5q"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"b0c82a3ade3497964cb8034be915da179459287823d92b5717e6d642784c50e6"},"ver":"1"}',
+    // '{"reqSignature":{},"txn":{"data":{"data":{"alias":"FoundationBuilder","blskey":"3gmhmqpPLqznZF3g3niodaHjbpsB6TEeE9SpgXgBnZJLmXgeRzJqTLajVwbhxrkomJFTFU4ohDC4ZRXKbUPCQywJuPAQnst8XBtCFredMECn4Z3goi1mNt5QVRdU8Ue2xMSkdLpsQMjCsNwYUsBguwXYUQnDXQXnHqRkK9qrivucQ5Z","blskey_pop":"RHWacPhUNc9JWsGNdmWYHrAvvhsow399x3ttNKKLDpz9GkxxnTKxtiZqarkx4uP5ByTwF4kM8nZddFKWuzoKizVLttALQ2Sc2BNJfRzzUZMNeQSnESkKZ7U5vE2NhUDff6pjANczrrDAXd12AjSG61QADWdg8CVciZFYtEGmKepwzP","client_ip":"35.161.146.16","client_port":"9702","node_ip":"50.112.53.5","node_port":"9701","services":["VALIDATOR"]},"dest":"GVvdyd7Y6hsBEy5yDDHjqkXgH8zW34K74RsxUiUCZDCE"},"metadata":{"from":"V5qJo72nMeF7x3ci8Zv2WP"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"fe991cd590fff10f596bb6fe2362229de47d49dd50748e38b96f368152be29c7"},"ver":"1"}',
   });
 
 const DEFAULT_LIBINDY_DID = 'LibindyDid211111111111';
@@ -93,4 +112,75 @@ export async function watch(
     globalCb(ledger, current);
   }
   foundationBuilderNode.close();
+}
+
+const defaultSizes = {
+  POOL: 40,
+  DOMAIN: 500,
+  CONFIG: 177,
+};
+
+async function definePoolSizes(): Promise<{ [key: string]: number }> {
+  const r = {};
+  const indy = await getIndy();
+  for (const ledger of ledgers) {
+    const tx = buildTx(ledger, 1);
+    const resp = await indy.send(tx);
+    r[ledger] = resp.result?.data?.ledgerSize || defaultSizes[ledger];
+  }
+  return r;
+}
+
+const FACTOR = 0.1;
+
+export class PoolerService {
+  public async Init(): Promise<void> {
+    console.time('LoadLedger');
+
+    const poolSizes = await definePoolSizes();
+
+    const int = setInterval(() => {
+      console.log(
+        `Loaded DOMAIN: [${parseFloat(
+          (
+            (db.DOMAIN.filter((tx: any) => tx && tx.result).length /
+              poolSizes['DOMAIN']) *
+            100
+          ).toString(),
+        ).toFixed(2)}%] and CONFIG: [${parseFloat(
+          (
+            (db.CONFIG.filter((tx: any) => tx && tx.result).length /
+              poolSizes['CONFIG']) *
+            100
+          ).toString(),
+        ).toFixed(2)}%]. ${sizeof(db.DOMAIN)}`,
+      );
+    }, 2000);
+
+    await Promise.all([
+      ...buildPromises(
+        (n) => queryPage('POOL', n),
+        poolSizes['POOL'],
+        Math.ceil(poolSizes['POOL'] * 0.5),
+      ),
+      ...buildPromises(
+        (n) => queryPage('CONFIG', n),
+        poolSizes['CONFIG'],
+        Math.ceil(poolSizes['CONFIG'] * FACTOR),
+      ),
+      ...buildPromises(
+        (n) => queryPage('DOMAIN', n),
+        poolSizes['DOMAIN'],
+        Math.ceil(poolSizes['DOMAIN'] * 0.01),
+      ),
+    ]);
+
+    clearInterval(int);
+
+    console.timeEnd('LoadLedger');
+
+    watch('DOMAIN', db.DOMAIN.length, 2000);
+    watch('POOL', db.POOL.length, 2000);
+    watch('CONFIG', db.CONFIG.length, 2000);
+  }
 }
--- Src/Services/PoCService.ts
+++ Src/Services/PoCService.ts
@@ -1,10 +1,9 @@
 import { HtmlServer } from '@Servers/HtmlServer';
 import { SocketServer } from '@Servers/SocketServer';
 import { ConfigSchema, ConfigService } from '@Services/ConfigService';
-import { watch, queryPage, db, setCb } from '@Services/Indy/PoolerService';
+import { PoolerService, db, setCb } from '@Services/Indy/PoolerService';
 import { RestAgency } from 'aries-sdk-ts-beta';
 import { URL } from 'url';
-import sizeof from 'object-sizeof';
 
 // import { Agency } from 'aries-sdk-ts';
 // import { PoolService } from '@Services/PoolService';
@@ -39,50 +38,8 @@ export class PoCService {
       publicDidSeed: '000000000000000000000000Steward3',
     });
 
-    const buildPromises = (cb, size, step = 50): any[] => {
-      return Array.from(
-        { length: Math.ceil(size / step) },
-        (v, i) => i * step,
-      ).map((i) => cb(i));
-    };
-
-    console.time('LoadLedger');
-
-    const int = setInterval(() => {
-      console.log(
-        `Loaded DOMAIN/${
-          db.DOMAIN.filter((tx: any) => tx && tx.result).length
-        } and CONFIG/${
-          db.CONFIG.filter((tx: any) => tx && tx.result).length
-        } records. ${sizeof(db.DOMAIN)}`,
-      );
-    }, 2000);
-
-    await Promise.all([
-      queryPage('POOL'),
-      ...buildPromises((n) => queryPage('CONFIG', n), 8000, 400),
-      ...buildPromises((n) => queryPage('DOMAIN', n), 60000, 1500),
-      // queryPage('CONFIG', 2),
-      // queryPage('DOMAIN', 100),
-      // queryPage('DOMAIN', 150),
-      // queryPage('DOMAIN', 200),
-      // queryPage('DOMAIN', 250),
-      // queryPage('DOMAIN', 300),
-      // queryPage('DOMAIN', 350),
-      // queryPage('DOMAIN', 400),
-      // queryPage('DOMAIN', 50),
-      // queryPage('DOMAIN'),
-    ]);
-
-    clearInterval(int);
-
-    console.timeEnd('LoadLedger');
-
-    console.log(`Loaded ${db.DOMAIN.length} records.}`);
-
-    watch('DOMAIN', db.DOMAIN.length, 2000);
-    watch('POOL', db.POOL.length, 2000);
-    watch('CONFIG', db.CONFIG.length, 2000);
+    const poolerservice = new PoolerService();
+    await poolerservice.Init();
 
     const ledgerExplorer = new SocketServer(
       'Ledger',
